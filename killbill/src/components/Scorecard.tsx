5c13557ed685d3aa2da252d54dc11004a1c48362
AsifShaikh01 <asifshaikh0024@gmail.com>
2023-04-09 17:05:07 +0530

    done

--- killbill/src/components/Scorecard.tsx
+++ killbill/src/components/Scorecard.tsx
@@ -14,7 +14,6 @@ const Scorecard = (props: Props) => {
     <div
       style={{
         display: "flex",
-        flexDirection:"column",
         alignItems: "center",
         padding: "10px",
         background: "black",
